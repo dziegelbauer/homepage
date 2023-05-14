c2fdd41901cef2abd67cf84fb25820c38bda1c5f
David Ziegelbauer <dziegelbauer@outlook.com>
2023-05-14 14:24:26 -0600

    Possible fix for image upload timeout

--- src/main/java/org/ziegelbauer/homepage/data/AWSS3Repository.java
+++ src/main/java/org/ziegelbauer/homepage/data/AWSS3Repository.java
@@ -1,25 +1,38 @@
 package org.ziegelbauer.homepage.data;
 
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
+@Slf4j
 @Repository
 public class AWSS3Repository {
-    public void uploadFile(String fileName, InputStream inputStream, String s3BucketName) {
+    public void uploadFile(String fileName, InputStream inputStream, String s3BucketName) throws IOException {
         AmazonS3Client client = (AmazonS3Client) AmazonS3Client.builder().build();
 
-        PutObjectRequest request = new PutObjectRequest(
+        byte[] imageData = inputStream.readAllBytes();
+        inputStream.close();
+
+        InputStream memoryStream = new ByteArrayInputStream(imageData);
+
+        ObjectMetadata metaData = new ObjectMetadata();
+
+        metaData.setContentLength(imageData.length);
+
+        log.info("Uploading file: {}", fileName);
+        client.putObject(
                 s3BucketName,
                 fileName,
-                inputStream,
-                null
+                memoryStream,
+                metaData
         );
-
-        client.putObject(request);
+        memoryStream.close();
+        log.info("Upload complete");
     }
 
     public void deleteFile(String fileName, String s3BucketName) {
